33785cca0b51dcf96de837108b864ff83841b27e
Claudio Muroni <muroni.claudio@gmail.com>
2025-01-27 19:03:22 +0100

    Built app skeleton

--- Main.py
+++ Main.py
@@ -1,9 +1,35 @@
-import os
+import settings
 from supabase import create_client, Client
 
-url: str = os.environ.get("ifesif_supabase_url")
-key: str = os.environ.get("ifesif_supabase_key")
-supabase: Client = create_client(url, key)
+supabase: Client = create_client(settings.url, settings.key)
 
-response = supabase.table("presidenti").select("*").execute()
-print(response)
+while True:
+    op = input("Digit operations:\nR (read)\nE (exit)\n-> ")
+    match op:
+        case "R" | "r" | "read":
+            op2 = input("Which table do you want to read?\nP (presidenti)\nC (contratti)\n-> ")
+            match op2:
+                case "P" | "p" | "presidenti":
+                    table = "presidenti"
+                    response = supabase.table(table).select("*").execute()
+                case "C" | "c" | "contratti":
+                    table = "contratti"
+                    op3 = input("Which president?\nCLA (Claudio)\nFLA (Flavio)\n-> ")
+                    match op3:
+                        case "CLA" | "cla":
+                            pres = "Claudio"
+                            response = supabase.table(table).select("ruolo","giocatore").eq("nome_presidente", pres).execute()
+                            print(response)
+                        case _:
+                            print(op3 + ": president not available")
+                case _:
+                    print(op2 + ": table not available")
+        
+        case "E" | "e" | "exit":
+            print("Terminate")
+            break
+            
+        case _:
+            print(op + ": operation not available")
+    
+
---
+++ settings.py
@@ -0,0 +1,4 @@
+import os
+
+url: str = os.environ.get("ifesif_supabase_url")
+key: str = os.environ.get("ifesif_supabase_key")
