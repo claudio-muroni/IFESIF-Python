323010c1ce1f688668e9c63460da4e98ee5c18ba
Claudio Muroni <muroni.claudio@gmail.com>
2025-02-12 19:57:44 +0100

    20250212: Switch season completed

--- switch_season.py
+++ switch_season.py
@@ -1,5 +1,6 @@
 import settings
 import utilities
+import math
 
 # PAGE
 
@@ -44,22 +45,54 @@ def next_season(supabase):
     op3 = input("\nDo you really want to go to the next season?\nYes\nNo\n-> ")
     match op3:
         case "Yes" | "yes":
+
+            # controlla che ci sia una sola stagione attiva
             response = supabase.table("stagioni").select("anno", count="exact").eq("attiva", True).execute()
             if response.count == 1:
                 anno = int(response.data[0]["anno"])
+                new_anno = anno+1
                 supabase.table("stagioni").update({"attiva":False}).eq("anno", anno).execute()
 
-                response = supabase.table("stagioni").select("anno", count="exact").eq("anno", anno+1).execute()
+                # attiva la stagione seguente (se non c'è creala)
+                response = supabase.table("stagioni").select("anno", count="exact").eq("anno", new_anno).execute()
                 if response.count > 0:
-                    supabase.table("stagioni").update({"attiva":True}).eq("anno", anno+1).execute()
+                    supabase.table("stagioni").update({"attiva":True}).eq("anno", new_anno).execute()
                 else:
                     new_season = {}
-                    new_season["anno"] = anno+1
+                    new_season["anno"] = new_anno
                     new_season["attiva"] = True
                     supabase.table("stagioni").insert(new_season).execute()
+                print(f"\nSeason successfully updated, current season {new_anno}")
 
-                print(f"\nSeason successfully updated, current season {anno+1}")
+                # aggiungi 500 crediti a tutti
+                response = supabase.table("presidenti").select("nome","cash").execute()
+                for i in range(len(response.data)):
+                    cash = response.data[i]["cash"]
+                    pres = response.data[i]["nome"]
+                    updated_cash = cash + 500
+                    new_cash = {}
+                    new_cash["cash"] = updated_cash
+                    supabase.table("presidenti").update(new_cash).eq("nome", pres).execute()
 
+                    contract_list = supabase.table("contratti").select("anno","prezzo", "giocatore").eq("nome_presidente", pres).execute()
+                    for j in range(len(contract_list.data)):
+                        anno_contratto = contract_list.data[j]["anno"]
+                        prezzo = contract_list.data[j]["prezzo"]
+                        giocatore = contract_list.data[j]["giocatore"]
+
+                        rinnovo = math.ceil(prezzo * (1 + 0.2*(new_anno - anno_contratto)))
+                        new_prezzo_rinnovo = {}
+                        new_prezzo_rinnovo["prezzo_rinnovo"] = rinnovo
+
+                        supabase.table("contratti").update(new_prezzo_rinnovo).eq("giocatore", giocatore).execute()
+                        
+                        updated_cash = updated_cash - rinnovo
+                        new_cash = {}
+                        new_cash["cash"] = updated_cash
+                        supabase.table("presidenti").update(new_cash).eq("nome", pres).execute()
+
+                print(f"Cash updated successfully for all presidents")
+                
         case "No" | "no":
             return
     
