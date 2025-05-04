f049b58278e8af53a73f1f0c2d9d67d98d812a9d
Claudio Muroni <muroni.claudio@gmail.com>
2025-05-04 18:08:03 +0200

    20250504: read ranking position added

--- read.py
+++ read.py
@@ -3,7 +3,7 @@ import pandas as pd
 # PAGE
 
 def read_page(supabase):
-    op2 = input("\nWhich table do you want to read?\nP (presidenti)\nC (contratti)\n-> ")
+    op2 = input("\nWhich table do you want to read?\nP (presidenti)\nC (contratti)\nR (rankings)\n-> ")
     match op2:
         case "P" | "p" | "presidenti":
             read_presidents(supabase)
@@ -20,6 +20,13 @@ def read_page(supabase):
                 
             else:
                 print(op3 + ": president not available")
+        
+        case "R" | "r" | "rankings":
+            year = input("Which year? -> ")
+            comp = input("Which competition? -> ")
+
+            read_rankings_for_year_competition(supabase, year, comp)
+            
 
         case _:
             print(op2 + ": table not available")
@@ -36,6 +43,13 @@ def read_presidents(supabase):
 
 def read_contracts_for_pres(supabase, pres):
     response = supabase.table("contratti").select("ruolo","giocatore","anno","prezzo","prezzo_rinnovo","durata").eq("nome_presidente", pres).execute()
+    df = pd.DataFrame(response.data)
+    print(df)
+    
+    return
+
+def read_rankings_for_year_competition(supabase, year, comp):
+    response = supabase.table("classifiche").select("*").eq("anno", year).eq("competizione", comp).execute()
     df = pd.DataFrame(response.data)
     print(df)
     
